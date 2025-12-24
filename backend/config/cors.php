ef638942d66ebf849d211aad221e116a88e46606
KOUASSI-Ken <kouassiken51@gmail.com>
2025-12-24 13:42:43 +0100

    Liaison du back et du front

---
+++ backend/config/cors.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'http://localhost:5173',  // Vite (dev)
+        'http://localhost:3000',  // Au cas où
+        'http://127.0.0.1:5173',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+];
